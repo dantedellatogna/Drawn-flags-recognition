b3ca1c5315e42a666813ae31ea39775309c7b903
dante <dellatogna.io@gmail.com>
2022-07-28 04:52:16 -0500

    Now the balls are drawn following the mouse movement

--- canvas.js
+++ canvas.js
@@ -1,49 +1,20 @@
-/*addEventListener("load", this)
-console.log("hola");
-
-const canvas = document.getElementById('canvas');
-
-if (canvas.getContext) {
-    const ctx = canvas.getContext('2d');
-
-    //cursor
-    var cursor = {
-        x: 0,
-        y: 0,
-        raidus: 3,
-        color: black,
-        draw(){
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.radius, 0, MATH.PI * 2, true);
-            ctx.closePath();
-            ctx.fillStyle = this.color;
-            ctx.fill();
-        }
-    }
-    
-}
-
-function clear(){
-    ctx.fillStyle = 'rgba(255, 255, 255, 0.3';
-    ctx.fillRect(0,0,canvas.width, canvas.height);
-}
-
-function draw(){
-    cursor.draw();
-}
-*/
 var canvas = document.getElementById("myCanvas")
 var ctx = canvas.getContext('2d')
 var raf;
+var rect = canvas.getBoundingClientRect();
 var running = false;
 
-var ball = {
+var clearButton = document.getElementById("clearButton");
+clearButton.addEventListener("click", clear())
+
+
+var cursor = {
   x: 100,
   y: 100,
-  vx: 5,
-  vy: 1,
-  radius: 25,
-  color: 'blue',
+  vx: 0,
+  vy: 0,
+  radius: 5,
+  color: 'black',
   draw() {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
@@ -53,46 +24,66 @@ var ball = {
   }
 };
 
+clear();
+
 function clear() {
   ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
   ctx.fillRect(0,0,canvas.width,canvas.height);
 }
 
 function draw() {
-  clear();
-  ball.draw();
-  ball.x += ball.vx;
-  ball.y += ball.vy;
+  //clear();
+  cursor.draw();
+  /*cursor.x += cursor.vx;
+  cursor.y += cursor.vy;
 
-  if (ball.y + ball.vy > canvas.height || ball.y + ball.vy < 0) {
-    ball.vy = -ball.vy;
+  if (cursor.y + cursor.vy > canvas.height || cursor.y + cursor.vy < 0) {
+    cursor.vy = -cursor.vy;
   }
-  if (ball.x + ball.vx > canvas.width || ball.x + ball.vx < 0) {
-    ball.vx = -ball.vx;
-  }
+  if (cursor.x + cursor.vx > canvas.width || cursor.x + cursor.vx < 0) {
+    cursor.vx = -cursor.vx;
+  }*/
 
   raf = window.requestAnimationFrame(draw);
 }
 
 canvas.addEventListener('mousemove', function(e) {
   if (!running) {
-    clear();
-    ball.x = e.clientX;
-    ball.y = e.clientY;
-    ball.draw();
+    //clear();
+    cursor.x = e.clientX - rect.left;
+    cursor.y = e.clientY - rect.top;
+    cursor.draw();
   }
 });
 
-canvas.addEventListener('click', function(e) {
+/*canvas.addEventListener('click', function(e) {
   if (!running) {
     raf = window.requestAnimationFrame(draw);
     running = true;
   }
-});
+});*/
+
+canvas.addEventListener('mousedown', function(e) {
+    if (!running) {
+      raf = window.requestAnimationFrame(draw);
+      running = true;
+    }
+  });
 
 canvas.addEventListener('mouseout', function(e) {
   window.cancelAnimationFrame(raf);
   running = false;
 });
 
-ball.draw();
+cursor.draw();
+
+//-----------------------------
+let screenLog = document.querySelector('#screen-log');
+document.addEventListener('mousemove', logKey);
+
+function logKey(e) {
+  screenLog.innerText = `
+    Screen X/Y: ${e.screenX}, ${e.screenY}
+    Client X/Y: ${e.clientX}, ${e.clientY}
+    Screen X/Y: ${cursor.x}, ${cursor.y}`;
+}
